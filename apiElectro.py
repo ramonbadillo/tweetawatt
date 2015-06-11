7fcbb7563fb4d4f5c96c430d9717210881838703
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2015-06-11 18:23:17 -0500

    lost data funtionallity added

--- apiElectro.py
+++ apiElectro.py
@@ -1,12 +1,20 @@
 import json
-import urllib2
+import urllib2 #library to POST in the API
+import os.path #library to read and delete files
+import sys
+import pickle
 from time import gmtime, strftime
+
+from pprint import pprint
+
 
 
 devUrl = ""
 data = {}
 user = ""
 recordsUrl = ""
+#file name of the lost Data
+fname = 'lostData.json'
 
 class apiElectro(object):
 
@@ -20,21 +28,91 @@ class apiElectro(object):
 
 
     def postElectroRegistry(self,watts,amp,volts,idKill,kwh,idDev):
+        #Create a dictionary
         data['watts'] = watts
         data['kwh'] = kwh
         data['amp'] = amp
         data['volts'] = volts
         data['idKill'] = idKill
-        data['timeStampClient'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        data['timeStampClient'] = strftime("%Y-%m-%d %H:%M:%S", gmtime()) #takes the time of the client and format to post in the API
         data['idDev'] = self.devUrl + idDev + "/"
         data['user'] = self.user
+
 
         self.postTo(self.recordsUrl,data)
 
 
     def postTo(self,url,data):
+        #try to POST in the server
+        try:
 
-        req = urllib2.Request(url)
-        req.add_header('Content-Type','application/json')
-        jdata = json.dumps(data)
-        response = urllib2.urlopen(req,jdata)
+            req = urllib2.Request(url)
+            req.add_header('Content-Type','application/json')
+            jdata = json.dumps(data)
+            response = urllib2.urlopen(req,jdata)
+
+
+            #check if the file exists, and POST every single item in the Server
+            if os.path.isfile(fname):
+                #open a file for read
+                file_object = open(fname).read()
+                #Converts the file object in a jsonObject
+                jsonFile = json.loads(file_object)
+
+                #Iterate the json file for a single item
+                for record in jsonFile:
+                    record['realTime'] = "false" #adds the field realTime and sets on FALSE
+                    #POST the record in the Server
+                    req = urllib2.Request(url)
+                    req.add_header('Content-Type','application/json')
+                    jdata = json.dumps(record)
+                    response = urllib2.urlopen(req,jdata)
+
+                #Delete the file when finish
+                os.remove(fname)
+
+        #Catch the exception when the conection doesnt exists
+        except:
+            print "Error:", sys.exc_info()
+
+            jsonFile = []
+            if os.path.isfile(fname):
+                #open a file for read
+                file_object = open(fname).read()
+                #Converts the file object in a jsonObject
+                jsonFile = json.loads(file_object)
+                #Adds the new record to a lost Data
+            jsonFile.extend(data)
+            pprint(jsonFile)
+            # Open a file for writing
+            out_file = open(fname,"a")
+
+            # Save the dictionary into this file
+            # (the 'indent=4' is optional, but makes it more readable)
+            #pickle.dump(jsonFile, out_file)
+            json.dump(data,out_file, indent=4)
+
+            # Close the file
+            out_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#THE END :)
