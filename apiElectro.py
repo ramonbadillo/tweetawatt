6c957a176df4cd80a653825bca5645e8937aed2a
Víctor Pablo <vickirin@gmail.com>
2015-06-29 14:27:20 -0500

    cambios a la api electro para tener mas loggeo y saber si hay problemas en alguna parte

--- apiElectro.py
+++ apiElectro.py
@@ -53,11 +53,14 @@ class apiElectro(object):
 
             #check if the file exists, and POST every single item in the Server
             if os.path.isfile(fname):
+                logging.debug('Inicia la busqueda del archivo de datos perdidos')
                 if os.path.isfile(fname):
                     #open a file for read
+                    logging.debug('Se encontro el archivo de datos perdidos')
                     file_object = open(fname).read()
                     #Converts the file object in a jsonObject
                     jsonFile = json.loads(file_object)
+                    logging.debug('Se cargaron en memoria los archivos de datos perdidos')
 
                     #Iterate the json file for a single item
                     for record in jsonFile:
@@ -67,36 +70,45 @@ class apiElectro(object):
                         req.add_header('Content-Type','application/json')
                         jdata = json.dumps(record)
                         response = urllib2.urlopen(req,jdata)
+                        logging.debug('Se envia al servidor un registro de datos perdidos')
 
                 #Delete the file when finish
+                logging.debug('Se va a borrar el archivo de datos perdidos')
                 os.remove(fname)
+                logging.debug('El archivo de datos perdidos ha sido borrado')
 
         #Catch the exception when the conection doesnt exists
         except  (urllib2.URLError, httplib.HTTPException) as e:
-            print e
+            logging.error('error al postear al servidor, %s',str(e))
 
             jsonFile = []
+            logging.debug('Inicia proceso de guardado de datos en archivo temporal')
             if os.path.isfile(fname):
 
                 if os.stat(fname).st_size != 0:
+                    logging.debug('Se encontro un archivo con datos ya existentes, se va a cargar')
                     #open a file for read
                     file_object = open(fname).read()
                     #Converts the file object in a jsonObject
                     jsonFile = json.loads(file_object)
 
-
+            logging.debug('Se van a agregar datos al archivo')
             #Adds the new record to a lost Data
             jsonFile.append(data)
+
 
             # Open a file for writing
             out_file = open(fname,"wb")
 
             # Save the dictionary into this file
             # (the 'indent=4' is optional, but makes it more readable)
+            logging.debug('Se va a escribir el archivo con datostemporales')
             json.dump(jsonFile,out_file, indent=4)
 
             # Close the file
+            logging.debug('se va a cerrar el archivo con datos temporales')
             out_file.close()
+            logging.debug('se ha cerrado el archivo con datos temporales')
 
 
 
